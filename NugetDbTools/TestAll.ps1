a961e8794d33d955614cae2876cef719809a477f
JamieO53 <jamieoglethorpe@gmail.com>
2017-10-26 10:54:31 +0200

    Clear previous test results and launch web page

--- NugetDbTools/TestAll.ps1
+++ NugetDbTools/TestAll.ps1
@@ -3,25 +3,29 @@ $SolutionPath = ls "$SolutionFolder\*.sln"
 if (-not (Get-Module NuGetShared)) {
 	Import-Module "$SolutionFolder\NugetShared\bin\Debug\NuGetShared\NuGetShared.psd1"
 }
-if (-not (Test-Path "$SolutionFolder\TestResults\HTML")) {
-	md "$SolutionFolder\TestResults\HTML"
+if (Test-Path "$SolutionFolder\TestResults") {
+	rmdir "$SolutionFolder\TestResults\*" -Recurse -Force
 }
+md "$SolutionFolder\TestResults\HTML"
+
 $allTests = @"
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <title>Results</title>
+    <title>NugetDbTools Tests</title>
   </head>
+  <body>
+	<h1>NugetDbTools test results</h1>
 	<ul>
 		$links
 	</ul>
-  <body>
   </body>
 </html>
 "@
 $links = ''
+
 Get-PowerShellProjects -SolutionPath $SolutionPath | % {
 	$projectFolder = Split-Path "$SolutionFolder\$($_.ProjectPath)"
 	if (Test-Path "$projectFolder\Tests")
@@ -59,4 +63,5 @@ $allTests = @"
   </body>
 </html>
 "@
-$allTests | Out-File "$SolutionFolder\TestResults\HTML\TestResults.html"
+$allTests | Out-File "$SolutionFolder\TestResults\HTML\TestResults.html" -Force
+iex "$SolutionFolder\TestResults\HTML\TestResults.html"
