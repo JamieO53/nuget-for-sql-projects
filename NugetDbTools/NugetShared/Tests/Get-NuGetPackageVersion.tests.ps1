ea7eb72a05e12660d80fcd4562fae0d2d0f6f1b4
JamieO53 <jamieoglethorpe@gmail.com>
2017-10-31 09:07:11 +0200

    Move Get-NuGetPackageVersion NugetDbTools -> NugetShared

---
+++ NugetDbTools/NugetShared/Tests/Get-NuGetPackageVersion.tests.ps1
@@ -0,0 +1,10 @@
+﻿if (Get-Module NugetShared) {
+	Remove-Module NugetShared
+}
+Import-Module "$PSScriptRoot\..\bin\Debug\NugetShared\NugetShared.psm1"
+
+Describe "Get-NuGetPackageVersion" {
+	Context "Existing package" {
+		It "Version" { Get-NuGetPackageVersion 'NuGetDbPacker.DbTemplate' | should not be '' }
+	}
+}
