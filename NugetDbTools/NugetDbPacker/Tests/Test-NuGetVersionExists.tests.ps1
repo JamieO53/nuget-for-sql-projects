f61ea18c53f392a7ca41cff022824b42e3007b9d
JamieO53 <jamieoglethorpe@gmail.com>
2017-11-08 08:08:58 +0200

    Fix version of test for existing NuGet project after deleting incorrectly versioned packages

--- NugetDbTools/NugetDbPacker/Tests/Test-NuGetVersionExists.tests.ps1
+++ NugetDbTools/NugetDbPacker/Tests/Test-NuGetVersionExists.tests.ps1
@@ -5,7 +5,7 @@ Import-Module "$PSScriptRoot\..\bin\Debug\NugetDbPacker\NugetDbPacker.psm1"
 
 Describe "Test-NuGetVersionExists" {
 	Context "Package Existance" {
-		It "Exists" { Test-NuGetVersionExists -Id NuGetDbPacker -Version 0.1.1 | should be $true }
+		It "Exists" { Test-NuGetVersionExists -Id NuGetDbPacker -Version 0.1.14 | should be $true }
 		It "Does not exist" { Test-NuGetVersionExists -Id NuGetDbPacker -Version 0.1.0 | should be $false }
 	}
 }
