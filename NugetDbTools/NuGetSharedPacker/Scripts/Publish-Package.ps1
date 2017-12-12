7401418128278444bc2571df584769aa46b986b3
JamieO53 <jamieoglethorpe@gmail.com>
2017-12-12 12:45:30 +0200

    Refactoring

---
+++ NugetDbTools/NuGetSharedPacker/Scripts/Publish-Package.ps1
@@ -0,0 +1,31 @@
+function Publish-Package {
+	<#.Synopsis
+	Publish a NuGet package for the project to the local NuGet server
+	.DESCRIPTION
+    Tests if the latest version of the project has been published.
+    
+    If not, a new package is created and is pushed to the NuGet server
+	.EXAMPLE
+	Publish-DbPackage -ProjectPath C:\VSTS\EcsShared\SupportRoles\EcsShared.SupportRoles.sqlproj
+	#>
+    [CmdletBinding()]
+    param
+    (
+        # The location of .sqlproj file of the project being published
+        [string]$ProjectPath
+	)
+    $configPath = [IO.Path]::ChangeExtension($ProjectPath, '.nuget.config')
+    $projFolder = Split-Path $ProjectPath -Resolve
+    $nugetFolder = [IO.Path]::Combine($projFolder, 'NuGet')
+    $settings = Import-NuGetSettings -NugetConfigPath $configPath
+    $id = $settings.nugetSettings.Id
+    $version = $settings.nugetSettings.version
+    if (-not (Test-NuGetVersionExists -Id $id -Version $version)) {
+        $nugetPackage = [IO.Path]::Combine($nugetFolder, "$id.$version.nupkg")
+        Initialize-Package -ProjectPath $ProjectPath
+        $source = Get-NuGetLocalSource
+        $apiKey = Get-NuGetLocalApiKey
+        nuget push $nugetPackage $apiKey -Source $source
+		Remove-NugetFolder $nugetFolder
+    }
+}
