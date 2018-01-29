2e207fb72981423675046927e75b4d1109401086
JamieO53 <jamieoglethorpe@gmail.com>
2018-01-29 11:22:04 +0200

    Push all packages

--- NugetDbTools/PackageAll.ps1
+++ NugetDbTools/PackageAll.ps1
@@ -2,10 +2,20 @@ if (git status --porcelain) {
 	Write-Error 'Commit changes before publishing the projects to NuGet'
 }
 else {
-	Remove-Variable * -ErrorAction SilentlyContinue
-	& '.\NugetShared\Package.ps1'
-	& '.\NugetSharedPacker\Package.ps1'
-	& '.\NugetDbPacker\Package.ps1'
-	& '.\NugetProjectPacker\Package.ps1'
-	& '.\DbSolutionBuilder\Package.ps1'
+	Remove-Variable * -ErrorAction Ignore
+	pushd '.\NugetShared'
+	& '.\Package.ps1'
+	popd
+	pushd '.\NugetSharedPacker'
+	& '.\Package.ps1'
+	popd
+	pushd '.\NugetDbPacker'
+	& '.\Package.ps1'
+	popd
+	pushd '.\NugetProjectPacker'
+	& '.\Package.ps1'
+	popd
+	pushd '.\DbSolutionBuilder'
+	& '.\Package.ps1'
+	popd
 }
