2e24fa2643c96f57f1638ce6cd1a4ad2d1b507e1
Jamie Oglethorpe <jamieoglethorpe@gmail.com>
2018-06-21 07:50:13 +0200

    Get the VSTS build agent branch

--- NugetDbTools/NugetShared/Scripts/Get-Branch.ps1
+++ NugetDbTools/NugetShared/Scripts/Get-Branch.ps1
@@ -7,7 +7,11 @@ function Get-Branch {
 	# Note: use Invoke-Expression (iex) so that git calls can be mocked in tests
 	try {
 		Push-Location $Path
-		if (Test-PathIsInGitRepo -Path (Get-Location)) {
+		# Check VSTS build agent branch
+		if ((Test-Path env:BUILD_SOURCEBRANCHNAME)) {
+			$branch = env:BUILD_SOURCEBRANCHNAME
+		}
+		elseif (Test-PathIsInGitRepo -Path (Get-Location)) {
 			$branch = iex 'git branch' | ? { $_.StartsWith('* ') } | % { $_.Replace('* ', '') }
 		}
 		else {
