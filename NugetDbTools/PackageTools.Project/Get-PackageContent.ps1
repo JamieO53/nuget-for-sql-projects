f7650505e18487a0bc2b03eb82c986f418487ff7
Jamie Oglethorpe <jamieoglethorpe@gmail.com>
2018-07-09 11:08:27 +0200

    Remove unused variable

--- NugetDbTools/PackageTools.Project/Get-PackageContent.ps1
+++ NugetDbTools/PackageTools.Project/Get-PackageContent.ps1
@@ -1,6 +1,5 @@
 $SolutionFolder = (Resolve-Path "$(Split-Path -Path $MyInvocation.MyCommand.Path)\..").Path
 [string]$slnPath=ls $SolutionFolder\*.sln | ? { $_ } | % { $_.FullName }
-$packageContentFolder = "$SolutionFolder\PackageContent"
 
 if (-not (Get-Module NugetProjectPacker)) {
 	Import-Module "$SolutionFolder\PowerShell\NugetProjectPacker.psd1"
