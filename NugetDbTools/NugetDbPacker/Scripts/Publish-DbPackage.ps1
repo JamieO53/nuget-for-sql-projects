96c8e24da14cac4ac6ef6dfc70c059badb1e1f84
Jamie Oglethorpe <jamieoglethorpe@gmail.com>
2018-10-17 12:17:52 +0200

    Solution path required when importing nuget settings

--- NugetDbTools/NugetDbPacker/Scripts/Publish-DbPackage.ps1
+++ NugetDbTools/NugetDbPacker/Scripts/Publish-DbPackage.ps1
@@ -19,7 +19,7 @@ function Publish-DbPackage {
     $configPath = [IO.Path]::ChangeExtension($ProjectPath, '.nuget.config')
     $projFolder = Split-Path $ProjectPath -Resolve
     $nugetFolder = [IO.Path]::Combine($projFolder, 'NuGet')
-    $settings = Import-NuGetSettings -NugetConfigPath $configPath
+    $settings = Import-NuGetSettings -NugetConfigPath $configPath -SolutionPath $SolutionPath
     $id = $settings.nugetSettings.Id
     $version = $settings.nugetSettings.version
     if (-not (Test-NuGetVersionExists -Id $id -Version $version)) {
