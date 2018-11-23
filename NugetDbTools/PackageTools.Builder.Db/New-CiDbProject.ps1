e7f541813049dc2015c0ad604b5c1109b5760591
Jamie Oglethorpe <jamieoglethorpe@gmail.com>
2018-11-23 13:41:57 +0200

    Remove duplicate PackageTools folders

--- NugetDbTools/PackageTools.Builder.Db/New-CiDbProject.ps1
+++
@@ -1,9 +0,0 @@
-param(
-	[string]$Path
-)
-if (Get-Module DbSolutionBuilder) {
-    Remove-Module DbSolutionBuilder
-}
-Import-Module .\PowerShell\DbSolutionBuilder.psd1
-$params = gc $Path
-New-DbSolution -Parameters $params
