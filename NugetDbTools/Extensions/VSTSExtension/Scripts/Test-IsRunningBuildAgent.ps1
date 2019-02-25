40c6a51d383998f66ebbad882ceaed9051f00fae
Jamie Oglethorpe <jamieoglethorpe@gmail.com>
2019-02-25 12:28:08 +0200

    Enabling CLR when deploying database

--- NugetDbTools/Extensions/VSTSExtension/Scripts/Test-IsRunningBuildAgent.ps1
+++ NugetDbTools/Extensions/VSTSExtension/Scripts/Test-IsRunningBuildAgent.ps1
@@ -1,10 +1,34 @@
 function Test-IsRunningBuildAgent {
-	$buildAgent = (
-		get-service | ? {
-			($_.Status -eq 'Running') -and ($_.Name -like 'vstsagent.*')
-		} | % {
-			$_.Name
+	if ($env:USERNAME -eq 'VssAdministrator') {
+		$true
+	} else {
+		$buildAgent = (
+			get-service | ? {
+				($_.Status -eq 'Running') -and ($_.Name -like 'vstsagent.*')
+			} | % {
+				$_.Name
+			}
+		)
+		-not ([string]::IsNullOrEmpty($buildAgent))
+	}
+}
+
+if (Test-IsRunningBuildAgent) {
+	$configPath = "$env:APPDATA\JamieO53\NugetDbTools\NugetDbTools.config"
+	$configFolder = Split-Path $configPath
+	if (-not (Test-Path $configPath)) {
+		$config = @"
+`<`?xml version=`"1.0`"`?`>
+`<configuration`>
+	`<nugetLocalServer`>
+		`<add key=`"ApiKey`" value=`"AzureDevOps`"`/`>
+		`<add key=`"Source`" value=`"$env:NuGetSource`"`/`>
+	`<`/nugetLocalServer`>
+`<`/configuration`>
+"@
+		if (-not (Test-Path $configFolder)) {
+			mkdir $configFolder | Out-Null
 		}
-	)
-	-not ([string]::IsNullOrEmpty($buildAgent))
+		$config | Out-File -FilePath $configPath -Encoding utf8
+	}
 }
