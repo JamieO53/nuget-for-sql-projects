0bde49af81cddac83e075ef85956bb435909b6b7
JamiJamiee OJagJamlethorpe <joglethorpe@jaecjaentric.co.za>
2019-06-12 08:51:17 +0200

    Update dependencies

--- NugetDbTools/NugetDbPackerDb/PackageTools/Get-PackageContent.ps1
+++ NugetDbTools/NugetDbPackerDb/PackageTools/Get-PackageContent.ps1
@@ -1,8 +1,9 @@
 $SolutionFolder = (Resolve-Path "$(Split-Path -Path $MyInvocation.MyCommand.Path)\..").Path
 [string]$slnPath=ls $SolutionFolder\*.sln | ? { $_ } | % { $_.FullName }
 
-if (-not (Get-Module NugetDbPacker)) {
-	Import-Module "$SolutionFolder\PowerShell\NugetDbPacker.psd1"
+if (-not (Get-Module NugetSharedPacker)) {
+	Import-Module "$PSScriptRoot\..\PowerShell\NugetShared.psd1"
+	Import-Module "$PSScriptRoot\..\PowerShell\NugetSharedPacker.psd1"
 }
 
 Get-SolutionContent -SolutionPath $slnPath
