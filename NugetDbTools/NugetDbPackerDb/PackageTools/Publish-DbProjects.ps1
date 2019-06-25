e2a63fe9cd3c56b90dce98012e95e8ca3a337e81
Jamie Oglethorpe <joglethorpe@ecentric.co.za>
2019-06-25 10:30:39 +0200

    Revert "Fix import module for Publish-DbProjects"
    This reverts commit 0ce87bed9b1e31ba25d7cdee1bebbd1f9f5ef85a.

--- NugetDbTools/NugetDbPackerDb/PackageTools/Publish-DbProjects.ps1
+++ NugetDbTools/NugetDbPackerDb/PackageTools/Publish-DbProjects.ps1
@@ -2,7 +2,6 @@ if ( Get-Module NugetDbPacker) {
 	Remove-Module NugetDbPacker
 }
 Import-Module "$PSScriptRoot\..\PowerShell\NugetDbPacker.psd1"
-Import-Module "$PSScriptRoot\..\PowerShell\NugetShared.psd1"
 
 $slnFolder = Get-ParentSubFolder "$PSScriptRoot" '*.sln'
 $slnPath = ls "$slnFolder\*.sln" | select -First 1 | % { $_.FullName }
