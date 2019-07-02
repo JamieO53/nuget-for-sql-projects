fcf7859d1ed09a5b93b877905f26eb1a662f36d2
Jamie Oglethorpe <joglethorpe@ecentric.co.za>
2019-07-02 11:42:36 +0200

    Use Runtime as content folder name in tests

--- NugetDbTools/NugetShared/Tests/Get-NuGetContentFolder.tests.ps1
+++ NugetDbTools/NugetShared/Tests/Get-NuGetContentFolder.tests.ps1
@@ -4,12 +4,22 @@
 Import-Module "$PSScriptRoot\..\bin\Debug\NugetShared\NugetShared.psm1"
 
 $Global:testing = $true
+#$config = @"
+#<?xml version="1.0"?>
+#<configuration>
+#	<nugetLocalServer>
+#		<add key="ApiKey" value="Test Key"/>
+#		<add key="ContentFolder" value="Content Folder"/>
+#		<add key="Source" value="Local Server"/>
+#	</nugetLocalServer>
+#</configuration>
+#"@
 $config = @"
 <?xml version="1.0"?>
 <configuration>
 	<nugetLocalServer>
 		<add key="ApiKey" value="Test Key"/>
-		<add key="ContentFolder" value="Content Folder"/>
+		<add key="ContentFolder" value="Runtime"/>
 		<add key="Source" value="Local Server"/>
 	</nugetLocalServer>
 </configuration>
@@ -21,7 +31,7 @@ Describe "Get-NuGetContentFolder" {
 	$config | Set-Content -Path $path
 	Context "Exists" {
 		It "Runs" {
-			Get-NuGetContentFolder | should be 'Content Folder'
+			Get-NuGetContentFolder | should be 'Runtime' #'Content Folder'
 		}
 	}
 }
