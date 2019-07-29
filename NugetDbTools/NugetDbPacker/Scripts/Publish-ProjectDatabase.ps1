e4cf856d29ac332989ca2c45116e19b0b664f438
Jamie Oglethorpe <joglethorpe@ecentric.co.za>
2019-07-29 07:42:24 +0200

    Add parameters to Publish-ProjectDatabase to override profile

--- NugetDbTools/NugetDbPacker/Scripts/Publish-ProjectDatabase.ps1
+++ NugetDbTools/NugetDbPacker/Scripts/Publish-ProjectDatabase.ps1
@@ -12,17 +12,27 @@ function Publish-ProjectDatabase {
         # The location of .dacpac file being published
 		[string]$DacpacPath,
         # The location of the profile (.publish.xml file being) with deployment options
-        [string]$ProfilePath
+        [string]$ProfilePath,
+		# Parameters overriding profile settings
+		# Format according to SqlPackage CLI https://docs.microsoft.com/en-us/sql/tools/sqlpackage?view=sql-server-2017
+		[string[]]$parameters
 	)
 	[string]$cmd = Find-SqlPackagePath
 	if ($cmd) {
 		try {
-	
+			if ($parameters) {
+				$params = [string]::Join(' ', $parameters)
+			} else {
+				$params = ''
+			}
 			if ($ProfilePath -and (Test-Path $ProfilePath)) {
 				[string]$db = "/pr:`"$ProfilePath`""
 			} else {
+				if (-not ($params.Contains('/p:CreateNewDatabase'))) {
+					$params += ' /p:CreateNewDatabase=True"'
+				}
 				$projectName = [IO.Path]::GetFileNameWithoutExtension($DacpacPath)
-				[string]$db = "/tdn:`"$projectName`" /p:CreateNewDatabase=True"
+				[string]$db = "/tdn:`"$projectName`" $params"
 			}
 	
 			Log "Publishing $DacpacPath using $ProfilePath"
