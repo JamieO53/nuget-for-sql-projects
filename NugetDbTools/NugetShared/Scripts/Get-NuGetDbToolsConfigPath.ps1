4ed69ec2ef72614973450654d848e2ecb38d34af
Jamie Oglethorpe <joglethorpe@ecentric.co.za>
2019-08-06 06:52:38 +0200

    Correct itentification of package tools configuration on hosted build server

--- NugetDbTools/NugetShared/Scripts/Get-NuGetDbToolsConfigPath.ps1
+++ NugetDbTools/NugetShared/Scripts/Get-NuGetDbToolsConfigPath.ps1
@@ -4,8 +4,8 @@ function Get-NuGetDbToolsConfigPath {
 	} else {
 		if (Test-Path "$env:APPDATA\JamieO53\NugetDbTools\NugetDbTools.config") {
 			"$env:APPDATA\JamieO53\NugetDbTools\NugetDbTools.config"
-		} elseif (Test-Path "$PSScriptRoot\..\JamieO53\NugetDbTools\JamieO53\NugetDbTools\NugetDbTools.config") {
-			"$PSScriptRoot\..\JamieO53\NugetDbTools\JamieO53\NugetDbTools\NugetDbTools.config"
+		} elseif (Test-Path "$PSScriptRoot\..\JamieO53\NugetDBTools\NugetDbTools.config") {
+			"$PSScriptRoot\..\JamieO53\NugetDBTools\NugetDbTools.config"
 		} else {
 			Log "Unable to find NuGetDbTools configuration"
 		}
