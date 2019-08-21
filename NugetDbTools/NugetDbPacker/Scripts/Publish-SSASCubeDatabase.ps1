e525159f37d7f811ae338dd2b95ee893f1c9e0eb
Jamie Oglethorpe <joglethorpe@ecentric.co.za>
2019-08-21 07:05:22 +0200

    Specify Cube Server version

--- NugetDbTools/NugetDbPacker/Scripts/Publish-SSASCubeDatabase.ps1
+++ NugetDbTools/NugetDbPacker/Scripts/Publish-SSASCubeDatabase.ps1
@@ -24,6 +24,19 @@ function Publish-SSASCubeDatabase {
 			-SSASCubeConfigSettingsPath "$CubeFolder\$CubeName.configsettings" `
 			-DataSourceConfigPath "$ConfigFolder\DataSources.configsettings"
 
-		Invoke-Trap -Command ".\Microsoft.AnalysisServices.Deployment.ps1 `"$CubeFolder\$CubeName.asdatabase`" `"$DatabaseName`"" -Message $DeploymentError -Fatal
+		[string]$TargetServerVersion = "2016"
+
+		if(Test-Path -Path "$ConfigFolder\AdditionalSettings.xml") {
+		    [xml]$SSASCubeAdditionalSettingsFile = Get-Content "$ConfigFolder\AdditionalSettings.xml"
+			if (-not $SSASCubeAdditionalSettingsFile.Settings.Server.Version) {
+				Write-Host "SSAS Target server version variable not defined, defaulting to $TargetServerVersion" -fore red
+			} else {
+				$TargetServerVersion = SSASCubeAdditionalSettingsFile.Settings.Server.Version;
+			}
+		} else {
+		    Write-Host "Additional settings file was not defined for this target deployment channel" -fore yellow
+		}
+		
+		Invoke-Trap -Command ".\Microsoft.AnalysisServices.Deployment.ps1 `"$CubeFolder\$CubeName.asdatabase`" `"$DatabaseName`" `"$TargetServerVersion`"" -Message $DeploymentError -Fatal
 	}
 }
