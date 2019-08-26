f5fdc0033795e599619a35ea30c9ea265654970d
Jamie Oglethorpe <joglethorpe@ecentric.co.za>
2019-08-26 08:37:41 +0200

    Hosted build server - configuration added to solution folder instead of appdata

--- NugetDbTools/NugetShared/Scripts/Get-NuGetDbToolsConfigPath.ps1
+++ NugetDbTools/NugetShared/Scripts/Get-NuGetDbToolsConfigPath.ps1
@@ -2,7 +2,13 @@ function Get-NuGetDbToolsConfigPath {
 	if ($Global:testing) {
 		"TestDrive:\Configuration\NugetDbTools.config"
 	} else {
-		"$env:APPDATA\JamieO53\NugetDbTools\NugetDbTools.config"
+		if (Test-Path "$PSScriptRoot\..\JamieO53\NugetDBTools\NugetDbTools.config") {
+			"$PSScriptRoot\..\JamieO53\NugetDBTools\NugetDbTools.config"
+		} elseif (Test-Path "$env:APPDATA\JamieO53\NugetDbTools\NugetDbTools.config") {
+			"$env:APPDATA\JamieO53\NugetDbTools\NugetDbTools.config"
+		} else {
+			Log "Unable to find NuGetDbTools configuration"
+		}
 	}
 }
 
