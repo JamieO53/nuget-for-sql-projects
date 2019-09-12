7685ec04f96bb7393e2391be1948138376d790a3
Jamie Oglethorpe <joglethorpe@ecentric.co.za>
2019-09-12 07:50:15 +0200

    Fix: Seek Extensions configuration in Powershell parent folder - Release Runtime folder

---
+++ NugetDbTools/NugetShared/Tests/Get-ToolsConfiguration.tests.ps1
@@ -0,0 +1,15 @@
+﻿Describe "Get-ToolsConfiguration" {
+	Context "Default test location" {
+		if (Get-Module NugetShared) {
+			Remove-Module NugetShared
+		}
+		Import-Module "$PSScriptRoot\..\bin\Debug\NugetShared\NugetShared.psm1" -Global -DisableNameChecking
+		It "Exists" {
+			Get-Item function:Get-ToolsConfiguration | should not be $null
+		}
+		$config = Get-ToolsConfiguration
+		It "Has content" {
+			$config | % {$_.rubbish} | should be 'junk'
+		}
+	}
+}
