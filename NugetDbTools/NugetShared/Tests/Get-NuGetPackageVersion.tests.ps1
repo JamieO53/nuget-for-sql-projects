67a25be939ab7e99e091ce326bcda50a92eecbf2
Jamie Oglethorpe <joglethorpe@ecentric.co.za>
2019-09-17 08:10:21 +0200

    Move Get-NuGetPackageVersion tests from NuGetSharedPacker to NuGetShared

---
+++ NugetDbTools/NugetShared/Tests/Get-NuGetPackageVersion.tests.ps1
@@ -0,0 +1,18 @@
+﻿Describe "Get-NuGetPackageVersion" {
+	if (-not (Get-Module TestUtils -All)) {
+		Import-Module "$PSScriptRoot\..\..\TestUtils\bin\Debug\TestUtils\TestUtils.psd1" -Global -DisableNameChecking
+	}
+	Describe "Test-NuGetVersionExists" {
+		$config = @"
+<?xml version="1.0"?>
+<tools>
+	<nuget>
+		<source>$env:USERPROFILE\.nuget\packages</source>
+	</nuget>
+</tools>
+"@
+	Initialize-NuGetSharedConfig $PSScriptRoot $config
+	Context "Existing package" {
+		It "Version" { Get-NuGetPackageVersion 'NuGetDbPacker.DbTemplate' | should not be '' }
+	}
+}
