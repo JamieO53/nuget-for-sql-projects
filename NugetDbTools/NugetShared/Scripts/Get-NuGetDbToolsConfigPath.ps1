cff57c6f2ea1283974ffff110c6f1a6acec45131
Jamie Oglethorpe <joglethorpe@ecentric.co.za>
2019-09-27 11:43:48 +0200

    Add $Global:ConfigPath to override default path locations

--- NugetDbTools/NugetShared/Scripts/Get-NuGetDbToolsConfigPath.ps1
+++ NugetDbTools/NugetShared/Scripts/Get-NuGetDbToolsConfigPath.ps1
@@ -1,17 +1,20 @@
 function Get-NuGetDbToolsConfigPath {
-	$configPath = "$PSScriptRoot\..\PackageTools\PackageTools.root.config"
-	if (-not (Test-Path $configPath)) {
-		## Release - look parent folder
-		$configPath = "$PSScriptRoot\..\PackageTools.root.config"
+	if ($Global:ConfigPath -and (Test-Path $Global:ConfigPath)) {
+		$configPath = $Global:ConfigPath
+	} else {
+		$configPath = "$PSScriptRoot\..\PackageTools\PackageTools.root.config"
 		if (-not (Test-Path $configPath)) {
-			## Testing - look in test scripts folder
-			$configPath = "$PSScriptRoot\..\..\..\Tests\PackageTools.root.config"
+			## Release - look parent folder
+			$configPath = "$PSScriptRoot\..\PackageTools.root.config"
 			if (-not (Test-Path $configPath)) {
-				Log 'Unable to find Package Tools configuration: PackageTools.root.config' -Error
-				Throw 'Missing configuration'
+				## Testing - look in test scripts folder
+				$configPath = "$PSScriptRoot\..\..\..\Tests\PackageTools.root.config"
+				if (-not (Test-Path $configPath)) {
+					Log 'Unable to find Package Tools configuration: PackageTools.root.config' -Error
+					Throw 'Missing configuration'
+				}
 			}
 		}
 	}
 	$configPath
 }
-
