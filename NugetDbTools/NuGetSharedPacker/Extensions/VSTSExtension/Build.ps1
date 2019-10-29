4a3f38c1bafd21307a1a63c37ba9f46500a9cf41
Jamie Oglethorpe <joglethorpe@ecentric.co.za>
2019-10-29 07:45:48 +0200

    GitHub compatibility changes

--- NugetDbTools/NuGetSharedPacker/Extensions/VSTSExtension/Build.ps1
+++ NugetDbTools/NuGetSharedPacker/Extensions/VSTSExtension/Build.ps1
@@ -7,16 +7,16 @@ $Dependents = @('NuGetSharedPacker', 'NugetDbPacker', 'DbSolutionBuilder', 'NuGe
 "'$SolutionDir\PowershellBuilder\PSModuleBuilder.ps1' -project $ProjectName -path $SolutionDir\NuGetSharedPacker\Extensions -outputPath $ProjectDir\bin\Debug"
 Push-Location $SolutionDir
 if (-not (Test-Path $ProjectDir\bin\Debug\$ProjectName)) {mkdir $ProjectDir\bin\Debug\$ProjectName | Out-Null}
-iex "$SolutionDir\PowershellBuilder\PSModuleBuilder.ps1 -project $ProjectName -path $SolutionDir\NuGetSharedPacker\Extensions -outputPath $ProjectDir\bin\Debug"
-popd
-copy "$ProjectDir\$ProjectName.psd1" "$ProjectDir\bin\Debug\$ProjectName"
-$Dependencies | % {
+Invoke-Expression "$SolutionDir\PowershellBuilder\PSModuleBuilder.ps1 -project $ProjectName -path $SolutionDir\NuGetSharedPacker\Extensions -outputPath $ProjectDir\bin\Debug"
+Pop-Location
+Copy-Item "$ProjectDir\$ProjectName.psd1" "$ProjectDir\bin\Debug\$ProjectName"
+$Dependencies | ForEach-Object {
 	if (Test-Path "$SolutionDir\$_\bin\Debug\$_") {
-		copy "$SolutionDir\$_\bin\Debug\$_\*" "$ProjectDir\bin\Debug\$ProjectName"
+		Copy-Item "$SolutionDir\$_\bin\Debug\$_\*" "$ProjectDir\bin\Debug\$ProjectName"
 	}
 }
-$Dependents | % {
+$Dependents | ForEach-Object {
 	if (Test-Path "$SolutionDir\$_\bin\Debug\$_") {
-		copy "$ProjectDir\bin\Debug\$ProjectName\*" "$SolutionDir\$_\bin\Debug\$_"
+		Copy-Item "$ProjectDir\bin\Debug\$ProjectName\*" "$SolutionDir\$_\bin\Debug\$_"
 	}
 }
